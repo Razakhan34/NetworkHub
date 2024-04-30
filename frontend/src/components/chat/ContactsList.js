06bd20d0826a3f888bf55f94de40887c6698196f
razakhan34 <rsanchit13@gmail.com>
2024-05-01 03:00:00 +0530

    First Commit

---
+++ frontend/src/components/chat/ContactsList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Contact from "./Contact";
+
+const ContactsList = ({ conversations }) => {
+  return (
+    <>
+      <div className="chat-contact-container">
+        {conversations &&
+          conversations.map((conversation, idx) => (
+            <Contact
+              key={conversation._id}
+              conversation={conversation}
+              lastIdx={idx === conversations.length - 1}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default ContactsList;
